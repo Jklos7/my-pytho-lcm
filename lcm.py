f6c9bc2fd357c4681dd5e7058405cfe90efeec28
Jklos7 <mjk744976@gmail.com>
2025-07-12 10:50:53 +0500

    first commit

---
+++ lcm.py
@@ -0,0 +1,13 @@
+def lcm(a, b):
+	max_num = max(a, b)
+	while True:
+		if max_num % a == 0 and max_num % b == 0:
+			lcm = max_num
+			break
+		max_num += 1
+	return lcm
+
+a = int(input("Enter your first number: "))
+b = int(input("Enter your second number: "))
+
+print(f"The LCM of {a} and {b} is {lcm(a, b)}")
